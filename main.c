f4f490bd428187701b90f5f760238edf25bb5445
Jon Driver <jon.a.driver.mail@gmail.com>
2026-06-21 16:57:27 +0100

    moved loop info into separate function call in main.c

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <time.h>
 #include <stdio.h>
 #include "event_loop_time.h"
+#include "rectangle.h"
 
 #define _POSIX_C_SOURCE 199309L // feature test macro to allow POSIX clock_gettime and CLOCK_MONOTONIC
 #define FPS 60
@@ -9,23 +10,28 @@
 // long long to handle the large counts from using nanoseconds
 
 
-int main( void ) {
-    int running = 1 ;
+void print_loop_info( void ) {
     int frame_count = 1 ;
     int second_count = 0 ;
 
+    if( frame_count == FPS ) {
+        frame_count = 1 ;
+        second_count++ ;
+    } else {
+        frame_count++ ;
+    }
+    
+    struct tm current_calendar_time = get_current_calendar_time() ;
+    printf( "%d - %d - %d\n", frame_count, second_count, current_calendar_time.tm_sec ) ;
+}
+
+
+int main( void ) {
+    int running = 1 ;
     while( running ) {
         long long frame_start_time = get_current_time_nanoseconds() ;
 
-        if( frame_count == FPS ) {
-            frame_count = 1 ;
-            second_count++ ;
-        } else {
-            frame_count++ ;
-        }
         
-        struct tm current_calendar_time = get_current_calendar_time() ;
-        printf( "%d - %d - %d\n", frame_count, second_count, current_calendar_time.tm_sec ) ;
 
         long long frame_elapsed_time = get_current_time_nanoseconds() - frame_start_time ;
         sleep_for_nanoseconds( FRAME_TIME_NANOSECONDS - frame_elapsed_time ) ;
