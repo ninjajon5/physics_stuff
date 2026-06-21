a96b4d8f67c9409983c9f6828197c18f50b347bc
Jon Driver <jon.a.driver.mail@gmail.com>
2026-06-21 16:58:43 +0100

    added gravity logs to main

--- main.c
+++ main.c
@@ -31,7 +31,9 @@ int main( void ) {
     while( running ) {
         long long frame_start_time = get_current_time_nanoseconds() ;
 
-        
+        struct rectangle rectangle = { .y = 0.0 } ;
+        rectangle_apply_gravity( rectangle, 1.0 ) ;
+        printf( "%f\n", rectangle.y ) ;
 
         long long frame_elapsed_time = get_current_time_nanoseconds() - frame_start_time ;
         sleep_for_nanoseconds( FRAME_TIME_NANOSECONDS - frame_elapsed_time ) ;
