9ad289f96739e3e991ea65a8ec16c3c2bab9db13
Jon Driver <jon.a.driver.mail@gmail.com>
2026-07-08 18:29:12 +0100

    added time info struct

--- utils/event_loop_time.h
+++ utils/event_loop_time.h
@@ -5,6 +5,14 @@
 returns timings for event loop
 */
 
+struct time_info {
+    int frame_count ;
+    int second_count ;
+    long long last_frame_start_time ;
+    long long frame_start_time ;
+    long long sleep_time ;
+} ;
+
 long long get_current_time_nanoseconds( void ) ;
 void sleep_for_nanoseconds( long long nanoseconds_to_sleep_for ) ;
 struct tm get_current_calendar_time( void ) ;
