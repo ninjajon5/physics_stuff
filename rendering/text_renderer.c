d15262948a062577cfe2502ef2cfe7743c0f5138
Jon Driver <jon.a.driver.mail@gmail.com>
2026-07-09 18:33:13 +0100

    added single threaded input polling

--- rendering/text_renderer.c
+++ rendering/text_renderer.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include "text_renderer.h"
 
+#ifdef _WIN32
+    #include <conio.h>
+#else
+    #include <unistd.h>
+    #include <sys/select.h>
+#endif
+
+char _text_renderer_get_input_character( void ) ;
+
 int text_renderer_init() {
     return 1 ;
 }
@@ -15,5 +24,28 @@ int text_renderer_quit_requested() {
 
 void text_renderer_shutdown( void ) {
     // no shutdown needed
+}
+
+char _text_renderer_get_input_character( void ) {
+    #ifdef _WIN32
+        if( _khbit() ) { // check the keyboard for input, otherwise return 0
+            return _getch() ; // read character from the buffer
+        }
+    #else
+        struct timeval tv = { 0, 0 } ; // needed for polling - set values to 0 to indicate no wait time
+        
+        fd_set fds ; // create a file descriptor sect
+        FD_ZERO( &fds ) ; // clear all file descriptors from the set
+        FD_SET( STDIN_FILENO, &fds ) ; // add standard input to the set
+
+        select( STDIN_FILENO + 1, &fds, NULL, NULL, &tv ) ; // monitor the set, reading it instantly
+        if( FD_ISSET( STDIN_FILENO, &fds ) ) { // check if the select() flagged the set as ready to read
+            char ch ;
+            if( read( STDIN_FILENO, &ch, 1 ) == 1 ) { // attempt to read the ch
+                return ch ; // if successful, return it
+            }
+        }
+    #endif
+        return 0 ;
 }
 
