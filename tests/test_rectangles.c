df8f2ef1b2857710add8db8f13abedc29a10dde6
ninjajon5 <jon.a.driver.mail@gmail.com>
2026-08-01 13:29:45 +0100

    both rectangle tests passing

--- tests/test_rectangles.c
+++ tests/test_rectangles.c
@@ -17,9 +17,33 @@ int _1_test_apply_gravity_increases_y_velocity( void ) {
     return 1 ;
 }
 
+int _2_test_apply_velocity_updates_position_correctly( void ) {
+    float starting_x = 0.0 ;
+    float starting_y = 0.0 ;
+    float starting_x_velocity = 1.0 ;
+    float starting_y_velocity = 2.0 ;
+
+    struct rectangle rectangle = {
+        .x = starting_x,
+        .y = starting_y,
+        .x_velocity = starting_x_velocity,
+        .y_velocity = starting_y_velocity
+    } ;
+
+    rectangle_apply_velocity( &rectangle ) ;
+
+    TASSERT(
+        rectangle.x > starting_x &&
+        rectangle.y > starting_y
+    ) ;
+
+    return 1 ;
+}
+
 
 test_function tests[] = {
-    _1_test_apply_gravity_increases_y_velocity
+    _1_test_apply_gravity_increases_y_velocity,
+    _2_test_apply_velocity_updates_position_correctly
 } ;
 int test_count = sizeof( tests ) / sizeof( tests[0] ) ;
 
